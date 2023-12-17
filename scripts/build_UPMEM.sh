4e04e517d4cadeb0f6234f1ccb932b4adec0b709
okudahi <pabrika@mineo.jp>
2023-12-18 08:54:16 +0900

    small

--- scripts/build_UPMEM.sh
+++ scripts/build_UPMEM.sh
@@ -5,5 +5,5 @@ echo cd $(pwd)
 # make binaries for UPMEM
 #rm -r ./build_UPMEM
 mkdir -p ./build_UPMEM
-cmake -S . -B ./build_UPMEM -DNR_TASKLETS=12 -DNUM_REQUESTS_PER_BATCH=1000000 -DNR_DPUS=500 -DNR_SEATS_IN_DPU=20 -DCMAKE_BUILD_TYPE=Debug ./build_UPMEM
+cmake -S . -B ./build_UPMEM -DNR_TASKLETS=12 -DNUM_REQUESTS_PER_BATCH=1000000 -DNR_DPUS=2500 -DNR_SEATS_IN_DPU=20 -DCMAKE_BUILD_TYPE=Release ./build_UPMEM
 cmake --build ./build_UPMEM
