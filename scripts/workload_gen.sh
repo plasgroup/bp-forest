36697cd36d4844f506e57f06f140b4446b5aa3e9
Tomoharu Ugawa <ugawa@ci.i.u-tokyo.ac.jp>
2023-12-28 23:50:16 +0900

    fix broken script

--- scripts/workload_gen.sh
+++ scripts/workload_gen.sh
@@ -4,9 +4,5 @@ echo cd $(pwd)
 mkdir -p workload
 for i in 0 0.4 0.6 0.99 1.2
 do
-<<<<<<< HEAD
     build/workload_gen/workload_gen -n 100000000 -a ${i} -e 2048
-=======
-    build_UPMEM/workload_gen/workload_gen -n 100000000 -a ${i} -e 2500
->>>>>>> run_upmem
 done
