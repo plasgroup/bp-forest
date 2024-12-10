e4e10db726d390284aedfaebe3fa7c1d264a5f8e
T.Hideshima <great.super.science@gmail.com>
2024-12-10 09:56:51 +0100

    auto commit

--- dpu/inc/sync.h
+++ dpu/inc/sync.h
@@ -7,10 +7,20 @@
 
 extern uint8_t __atomic_bit AtomicBits[NR_TASKLETS * 2];
 
+__attribute__((unused)) static void initialize_readiness_bits(void)
+{
+    for (unsigned i = 0; i < NR_TASKLETS * 2 - 1; i++) {
+        __asm__("acquire %[index], %[base], t, .+1" ::[index] "r"(i),
+                [base] "i"(&AtomicBits)
+                :);
+    }
+}
+
 // AtomicBits[0, NR_TASKLETS - 1)
 __attribute__((unused)) static void notify_next_of_readiness(void)
 {
-    __asm__("acquire id, %[base], t, .+1\n"
+    // __asm__("acquire id, %[base], t, .+1\n"
+    __asm__("release id, %[base], nz, .+1\n"
             // "resume id, 1" ::[base] "i"(&AtomicBits)
             "" ::[base] "i"(&AtomicBits)
             :);
@@ -18,16 +28,19 @@ __attribute__((unused)) static void notify_next_of_readiness(void)
 __attribute__((unused)) static void wait_for_prev_ready(void)
 {
     __asm__("0:\n"
-            "release id, %[base] - 1, nz, .+2\n"
+            // "release id, %[base] - 1, nz, .+2\n"
+            "acquire id, %[base] - 1, nz, .\n"
             // "stop true, 0b" ::[base] "i"(&AtomicBits)
-            "jump 0b" ::[base] "i"(&AtomicBits)
+            // "jump 0b" ::[base] "i"(&AtomicBits)
+            "" ::[base] "i"(&AtomicBits)
             :);
 }
 
 // AtomicBits[NR_TASKLETS - 1, 2 * NR_TASKLETS - 2)
 __attribute__((unused)) static void notify_prev_of_readiness(void)
 {
-    __asm__("acquire id, %[base] + %[nr_tasklets] - 2, true, .+1\n"
+    // __asm__("acquire id, %[base] + %[nr_tasklets] - 2, true, .+1\n"
+    __asm__("release id, %[base] + %[nr_tasklets] - 2, nz, .+1\n"
             // "resume id, -1" ::[base] "i"(&AtomicBits),
             "" ::[base] "i"(&AtomicBits),
             [nr_tasklets] "i"(NR_TASKLETS)
@@ -36,9 +49,11 @@ __attribute__((unused)) static void notify_prev_of_readiness(void)
 __attribute__((unused)) static void wait_for_next_ready(void)
 {
     __asm__("0:\n"
-            "release id, %[base] + %[nr_tasklets] - 1, nz, .+2\n"
+            // "release id, %[base] + %[nr_tasklets] - 1, nz, .+2\n"
+            "acquire id, %[base] + %[nr_tasklets] - 1, nz, .\n"
             // "stop true, 0b" ::[base] "i"(&AtomicBits),
-            "jump 0b" ::[base] "i"(&AtomicBits),
+            // "jump 0b" ::[base] "i"(&AtomicBits),
+            "" ::[base] "i"(&AtomicBits),
             [nr_tasklets] "i"(NR_TASKLETS)
             :);
 }
--- dpu/src/dpumain.c
+++ dpu/src/dpumain.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "input_header.h"
+#include "sync.h"
 #include "tree.h"
 
 #include <barrier.h>
@@ -20,6 +21,7 @@ int main()
 {
     if (me() == LAST_TASKLET) {
         mram_read(DPU_MRAM_HEAP_POINTER, &input_header, sizeof(input_header));
+initialize_readiness_bits();
 printf("task_no = %d\n", input_header.task_no);
     }
     barrier_wait(&my_barrier);
