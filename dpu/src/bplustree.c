0065ff74149ee1c8fac6ef223db863434a3deeb6
T.Hideshima <great.super.science@gmail.com>
2024-12-10 16:51:04 +0100

    auto commit

--- dpu/src/bplustree.c
+++ dpu/src/bplustree.c
@@ -1094,7 +1094,7 @@ for (unsigned i = 0; i < stack_height; i++) {
                     Free_node(cursor.ptr);
 
 static unsigned loop_count = 0;
-if (loop_count++ >= 1000) {
+if (loop_count++ >= 1500) {
     return;
 }
                     for (;;) {
