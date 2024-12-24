bbe7da1a246ba3f3286122f9ac24b8afafbf91b5
Tomoharu Ugawa <ugawa@ci.i.u-tokyo.ac.jp>
2024-12-25 02:22:28 +0900

    refactoring

--- cpudb/main.cpp
+++ cpudb/main.cpp
@@ -8,12 +8,9 @@
 #include "host/inc/statistics.hpp"
 #include "piecewise_constant_workload.hpp"
 #include "sparsetable.ipp"
-#include "host/inc/workload_buffer.hpp"
+#include "workload_buffer.hpp"
 
 std::chrono::nanoseconds QueryProcessTime;
-
-// temporary
-size_t range_length;
 
 struct Option {
     void parse(int argc, char* argv[])
@@ -184,57 +181,35 @@ Database* make_database(size_t nr_keys, bool is_single_thread)
     return db;
 }
 
-void do_one_batch(const uint64_t task, int batch_num,
-                  WorkloadBuffer& workload_buffer,
-                  Database* db)
-{
-    const auto tmp_input = workload_buffer.take(NUM_REQUESTS_PER_BATCH);
-    const auto batch_keys = tmp_input.first;
-    const auto num_keys_batch = tmp_input.second;
-    if (num_keys_batch != NUM_REQUESTS_PER_BATCH) {
-        std::cerr << "run out of workload in batch " << batch_num << std::endl;
-        exit(1);
-    }
-
-    if (task == TASK_GET) {
-        static ExtendableBuffer<value_uint64_t> results;
-        static ExtendableBuffer<key_uint64_t> keys;
-        keys.reserve(num_keys_batch);
-        for (size_t idx_query = 0; idx_query < num_keys_batch; idx_query++)
-            keys[idx_query] = batch_keys[idx_query];
-        results.reserve(num_keys_batch);
-        {
-            StopWatch sw(QueryProcessTime);
-            db->batch_get(num_keys_batch, keys, results);
-        }
-    } else if (task == TASK_RANGE_MIN) {
-        static ExtendableBuffer<value_uint64_t> results;
-        static ExtendableBuffer<KeyRange> ranges;
-        ranges.reserve(num_keys_batch);
-        for (size_t idx_query = 0; idx_query < num_keys_batch; idx_query++) {
-            ranges[idx_query].begin = batch_keys[idx_query];
-            ranges[idx_query].end = ranges[idx_query].begin + INIT_KEY_INTERVAL * 100 - 1;
-        }
-        results.reserve(num_keys_batch);
-        {
-            StopWatch sw(QueryProcessTime);
-            db->batch_range_minimum(num_keys_batch, ranges, results);
-        }
-    } else {
-        std::cerr << "unsupported task type: " << task << std::endl;
-        exit(1);
-    }
-}
-#if 0
 class Benchmark {
 public:
     void run(int nr_batches, Database* db)
     {
         for (int idx_batch = 0; idx_batch < nr_batches; idx_batch++) {
             do_one_batch(idx_batch, db);
+            printf("%s,%d,%d,%d,%d,%ld\n",
+                opt.alpha.c_str(), 1, db->get_parallelism(), idx_batch,
+                NUM_REQUESTS_PER_BATCH, QueryProcessTime.count());
         }
     }
+    virtual ~Benchmark() {}
     virtual void do_one_batch(int idx_batch, Database* db) = 0;
+
+    template <typename T>
+    size_t prepare_buffer(int idx_batch, WorkloadBuffer<T>* workload_buffer, ExtendableBuffer<T>& queires, ExtendableBuffer<value_uint64_t>& results) {
+        const auto tmp_input = workload_buffer->take(NUM_REQUESTS_PER_BATCH);
+        const auto batch_queries = tmp_input.first;
+        const auto num_queries_batch = tmp_input.second;
+        if (num_queries_batch != NUM_REQUESTS_PER_BATCH) {
+            std::cerr << "run out of workload in batch " << idx_batch << std::endl;
+            exit(1);
+        }
+        queires.reserve(num_queries_batch);
+        for (size_t idx_query = 0; idx_query < num_queries_batch; idx_query++)
+            queires[idx_query] = batch_queries[idx_query];
+        results.reserve(num_queries_batch);
+        return num_queries_batch;
+    }
 };
 
 class GetBenchmark : public Benchmark {
@@ -248,24 +223,18 @@ public:
         workload_buffer = new WorkloadBuffer<key_uint64_t>(std::move(workload.data));
     }
 
-    void do_one_batch(int idx_batch, Database* db) {
-        const auto tmp_input = workload_buffer->take(NUM_REQUESTS_PER_BATCH);
-        const auto batch_keys = tmp_input.first;
-        const auto num_keys_batch = tmp_input.second;
-        if (num_keys_batch != NUM_REQUESTS_PER_BATCH) {
-            std::cerr << "run out of workload in batch " << idx_batch << std::endl;
-            exit(1);
-        }
+    ~GetBenchmark()
+    {
+        delete workload_buffer;
+    }
 
+    void do_one_batch(int idx_batch, Database* db) {
         static ExtendableBuffer<value_uint64_t> results;
         static ExtendableBuffer<key_uint64_t> keys;
-        keys.reserve(num_keys_batch);
-        for (size_t idx_query = 0; idx_query < num_keys_batch; idx_query++)
-            keys[idx_query] = batch_keys[idx_query];
-        results.reserve(num_keys_batch);
+        size_t num_queries_batch = prepare_buffer(idx_batch, workload_buffer, keys, results);
         {
             StopWatch sw(QueryProcessTime);
-            db->batch_get(num_keys_batch, keys, results);
+            db->batch_get(num_queries_batch, keys, results);
         }
     }
 };
@@ -288,54 +257,48 @@ public:
         workload_buffer = new WorkloadBuffer<KeyRange>(std::move(workload));
     }
 
-    void do_one_batch(int idx_batch, Database* db) {
-        const auto tmp_input = workload_buffer->take(NUM_REQUESTS_PER_BATCH);
-        const auto batch_queries = tmp_input.first;
-        const auto num_queries_batch = tmp_input.second;
-        if (num_queries_batch != NUM_REQUESTS_PER_BATCH) {
-            std::cerr << "run out of workload in batch " << idx_batch << std::endl;
-            exit(1);
-        }
+    ~RMQBenchmark()
+    {
+        delete workload_buffer;
+    }
 
+    void do_one_batch(int idx_batch, Database* db) {
         static ExtendableBuffer<value_uint64_t> results;
         static ExtendableBuffer<KeyRange> ranges;
-        ranges.reserve(num_queries_batch);
-        for (size_t idx_query = 0; idx_query < num_queries_batch; idx_query++)
-            ranges[idx_query] = batch_queries[idx_query];
-        results.reserve(num_queries_batch);
+        size_t num_queries_batch = prepare_buffer(idx_batch, workload_buffer, ranges, results);
         {
             StopWatch sw(QueryProcessTime);
             db->batch_range_minimum(num_queries_batch, ranges, results);
         }
     }
 };
-#endif
-
 
 int main(int argc, char* argv[])
 {
     opt.parse(argc, argv);
 
-    // temporary
-    key_uint64_t key_interval = (KEY_MAX - KEY_MIN) / (opt.nr_keys - 1); 
-    range_length = key_interval * 100 - 1;
-
-    Database* db = make_database(opt.nr_keys, opt.is_single_thread);
-
-    PiecewiseConstantWorkload workload;
-    load_workload(opt.workload_file, &workload);
-    WorkloadBuffer workload_buffer{std::move(workload.data)};
-
-
-
-    for (int idx_batch = 0; idx_batch < opt.nr_batches; idx_batch++) {
-        do_one_batch(opt.op_type, idx_batch, workload_buffer, db);
-
-        printf("%s,%d,%d,%d,%d,%ld\n",
-                opt.alpha.c_str(), 1, db->get_parallelism(), idx_batch,
-                NUM_REQUESTS_PER_BATCH, QueryProcessTime.count());
+    Benchmark* benchmark;
+    if (opt.op_type == TASK_GET)
+        benchmark = new GetBenchmark(opt.workload_file);
+    else if (opt.op_type == TASK_RANGE_MIN)
+        benchmark = new RMQBenchmark(opt.workload_file);
+    else {
+        std::cerr << "unsupported task type: " << opt.op_type << std::endl;
+        exit(1);
     }
 
-    return 0;
+    Database* db;
+    std::chrono::nanoseconds DatabaseInitTime;
+    {
+        StopWatch sw(DatabaseInitTime);
+        db = make_database(opt.nr_keys, opt.is_single_thread);
+    }
+    std::cout << "database initialized in " << (DatabaseInitTime.count() / 1000 / 1000) << " ms" << std::endl;
 
+    benchmark->run(opt.nr_batches, db);
+
+    delete benchmark;
+    delete db;
+
+    return 0;
 }
