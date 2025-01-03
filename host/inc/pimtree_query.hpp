bef3492b06a8b2c7793bf47c69ec79d5f0ba4e65
ryuumorimoto <mryuu1128@gmail.com>
2025-01-03 17:14:49 +0900

    入力を扱うinterfaceを作成

---
+++ host/inc/pimtree_query.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+#include <iostream>
+
+enum operation_t {
+    empty_t,
+    get_t,
+    update_t,
+    predecessor_t,
+    scan_t,
+    insert_t,
+    remove_t
+};
+struct get_operation {
+    int64_t key;
+};
+struct update_operation {
+    int64_t key;
+    int64_t value;
+};
+struct predecessor_operation {
+    int64_t key;
+};
+
+struct scan_operation {
+    int64_t lkey;
+    int64_t rkey;
+};
+
+struct insert_operation {
+    int64_t key;
+    int64_t value;
+};
+
+struct remove_operation {
+    int64_t key;
+};
+
+struct operation {
+    union {
+        get_operation g;
+        update_operation u;
+        predecessor_operation p;
+        scan_operation s;
+        insert_operation i;
+        remove_operation r;
+    } tsk;
+    operation_t type;
+};
+
+struct pimtree_queries {
+    operation* ops;
+    size_t length;
+};
+
+inline pimtree_queries make_pimtree_queries(std::string filepath);
+inline void show_pimtree_queries(pimtree_queries queries);
