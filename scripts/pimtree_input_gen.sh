1f384b9085d966108c6103f0cbf6464c974e1583
T.Hideshima <great.super.science@gmail.com>
2025-01-09 18:05:14 +0900

    update script

--- scripts/pimtree_input_gen.sh
+++ scripts/pimtree_input_gen.sh
@@ -8,10 +8,10 @@ cd $PIM_TREE_DIR
 
 
 # パラメータは以下の通り
-dpu_nums=(64) #DPUの数(range queryの幅に相当？)
-initial_element_num=(1000000 1000000) # 初期状態の要素の数
-test_query_num=(100000000 100000000) # テスト時のqueryの数
-test_query_skews=(0.6 1.2) # the skew of testing queries(=alpha)
+dpu_nums=(500) #DPUの数(range queryの幅に相当？)
+initial_element_num=(100000000) # 初期状態の要素の数
+test_query_num=(20000000) # テスト時のqueryの数
+test_query_skews=(0.6) # the skew of testing queries(=alpha)
 
 # 初期状態の数, テスト時queryの数, skew配列の長さは揃える必要がある
 parameter_length=${#initial_element_num[@]}
