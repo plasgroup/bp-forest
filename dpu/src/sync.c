3e6d1283137f9c55b0e7241c45411435c17b772a
T.Hideshima <great.super.science@gmail.com>
2025-07-21 17:15:45 +0000

    avoid complex sync on DPUs

--- dpu/src/sync.c
+++ dpu/src/sync.c
@@ -4,3 +4,10 @@
 
 
 uint8_t __atomic_bit AtomicBits[NR_TASKLETS * 2];
+
+
+#include <mutex.h>
+
+MUTEX_INIT(tmp_sync_mutex);
+bool fwd_readiness[NR_TASKLETS] = {};
+bool bwd_readiness[NR_TASKLETS] = {};
